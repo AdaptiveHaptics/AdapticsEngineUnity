e5b9a56d4ea97b3872b5d6d42e19685d528aad78
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2023-06-22 22:16:50 -0700

    improve error reporting

--- Assets/AdapticsEngineMatrix.cs
+++ Assets/AdapticsEngineMatrix.cs
@@ -7,24 +7,36 @@ public class AdapticsEngineMatrix : MonoBehaviour
     private IntPtr engineHandle;
     void Awake()
     {
-        print(AdapticsEngineInterop.ffi_api_guard());
+        Debug.Log(AdapticsEngineInterop.ffi_api_guard());
         engineHandle = AdapticsEngineInterop.init_adaptics_engine(false);
-        print("init adaptics engine");
+        Debug.Log("init adaptics engine");
     }
 
     void Start()
     {
         var json_pattern = Resources.Load<TextAsset>("AdapticsTestPattern").text;
-        AdapticsEngineInterop.adaptics_engine_update_pattern(engineHandle, json_pattern);
-        print("loaded pattern");
+        AdapticsEngineInterop.adaptics_engine_update_pattern_checked(engineHandle, json_pattern);
+        Debug.Log("loaded pattern");
         double current_time_ms = (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds;
-        AdapticsEngineInterop.adaptics_engine_update_playstart(engineHandle, current_time_ms, 0);
-        print("updated playstart");
+        AdapticsEngineInterop.adaptics_engine_update_playstart_checked(engineHandle, current_time_ms, 0);
+        Debug.Log("updated playstart");
     }
     private void OnApplicationQuit()
     {
-        print("pre deinit adaptics engine");
-        AdapticsEngineInterop.deinit_adaptics_engine(engineHandle);
-        print("deinit adaptics engine");
+        Debug.Log("pre deinit_adaptics_engine");
+        byte[] err_msg = new byte[1024];
+        try
+        {
+            AdapticsEngineInterop.deinit_adaptics_engine(engineHandle, err_msg);
+        } catch (InteropException<FFIError> e)
+        {
+            if (e.Error == FFIError.ErrMsgProvided)
+            {
+                var err_msg_str = System.Text.Encoding.UTF8.GetString(err_msg).TrimEnd('\0');
+                throw new InvalidOperationException("deinit_adaptics_engine error: " + err_msg_str);
+            }
+            throw e;
+        }
+        Debug.Log("deinit_adaptics_engine");
     }
 }
--- Assets/Plugins/AdapticsEngineInterop.cs
+++ Assets/Plugins/AdapticsEngineInterop.cs
@@ -17,9 +17,9 @@ namespace com.github.AdaptiveHaptics
         static AdapticsEngineInterop()
         {
             var api_version = AdapticsEngineInterop.ffi_api_guard();
-            if (api_version != 15814009185797291274ul)
+            if (api_version != 356762228646146003ul)
             {
-                throw new TypeLoadException($"API reports hash {api_version} which differs from hash in bindings (15814009185797291274). You probably forgot to update / copy either the bindings or the library.");
+                throw new TypeLoadException($"API reports hash {api_version} which differs from hash in bindings (356762228646146003). You probably forgot to update / copy either the bindings or the library.");
             }
         }
 
@@ -30,27 +30,160 @@ namespace com.github.AdaptiveHaptics
         /// # Safety
         /// `handle` must be a valid pointer to an `AdapticsEngineHandleFFI` allocated by `init_adaptics_engine`
         [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "deinit_adaptics_engine")]
-        public static extern void deinit_adaptics_engine(IntPtr handle);
+        public static extern FFIError deinit_adaptics_engine(IntPtr handle, SliceMutu8 err_msg);
+
+        /// # Safety
+        /// `handle` must be a valid pointer to an `AdapticsEngineHandleFFI` allocated by `init_adaptics_engine`
+        public static void deinit_adaptics_engine(IntPtr handle, byte[] err_msg)
+        {
+            var err_msg_pinned = GCHandle.Alloc(err_msg, GCHandleType.Pinned);
+            var err_msg_slice = new SliceMutu8(err_msg_pinned, (ulong) err_msg.Length);
+            try
+            {
+                var rval = deinit_adaptics_engine(handle, err_msg_slice);;
+                if (rval != FFIError.Ok)
+                {
+                    throw new InteropException<FFIError>(rval);
+                }
+            }
+            finally
+            {
+                err_msg_pinned.Free();
+            }
+        }
 
         /// # Safety
         /// `handle` must be a valid pointer to an `AdapticsEngineHandle` allocated by `init_adaptics_engine`
         [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "adaptics_engine_update_pattern")]
-        public static extern void adaptics_engine_update_pattern(IntPtr handle, string pattern_json);
+        public static extern FFIError adaptics_engine_update_pattern(IntPtr handle, string pattern_json);
+
+        /// # Safety
+        /// `handle` must be a valid pointer to an `AdapticsEngineHandle` allocated by `init_adaptics_engine`
+        public static void adaptics_engine_update_pattern_checked(IntPtr handle, string pattern_json)
+        {
+            var rval = adaptics_engine_update_pattern(handle, pattern_json);;
+            if (rval != FFIError.Ok)
+            {
+                throw new InteropException<FFIError>(rval);
+            }
+        }
 
         /// # Safety
         /// `handle` must be a valid pointer to an `AdapticsEngineHandle` allocated by `init_adaptics_engine`
         [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "adaptics_engine_update_playstart")]
-        public static extern void adaptics_engine_update_playstart(IntPtr handle, double playstart, double playstart_offset);
+        public static extern FFIError adaptics_engine_update_playstart(IntPtr handle, double playstart, double playstart_offset);
+
+        /// # Safety
+        /// `handle` must be a valid pointer to an `AdapticsEngineHandle` allocated by `init_adaptics_engine`
+        public static void adaptics_engine_update_playstart_checked(IntPtr handle, double playstart, double playstart_offset)
+        {
+            var rval = adaptics_engine_update_playstart(handle, playstart, playstart_offset);;
+            if (rval != FFIError.Ok)
+            {
+                throw new InteropException<FFIError>(rval);
+            }
+        }
 
         /// # Safety
         /// `handle` must be a valid pointer to an `AdapticsEngineHandle` allocated by `init_adaptics_engine`
         [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "adaptics_engine_update_parameters")]
-        public static extern void adaptics_engine_update_parameters(IntPtr handle, string evaluator_params);
+        public static extern FFIError adaptics_engine_update_parameters(IntPtr handle, string evaluator_params);
 
+        /// # Safety
+        /// `handle` must be a valid pointer to an `AdapticsEngineHandle` allocated by `init_adaptics_engine`
+        public static void adaptics_engine_update_parameters_checked(IntPtr handle, string evaluator_params)
+        {
+            var rval = adaptics_engine_update_parameters(handle, evaluator_params);;
+            if (rval != FFIError.Ok)
+            {
+                throw new InteropException<FFIError>(rval);
+            }
+        }
+
+        /// Guard function used by backends.
+        ///
+        /// Change impl version in this comment to force bump the API version.
+        /// impl_version: 1
         [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ffi_api_guard")]
         public static extern ulong ffi_api_guard();
 
     }
+
+    public enum FFIError
+    {
+        Ok = 0,
+        NullPassed = 1,
+        Panic = 2,
+        OtherError = 3,
+        AdapticsEngineThreadDisconnectedCheckDeinitForMoreInfo = 4,
+        ErrMsgProvided = 5,
+    }
+
+    ///A pointer to an array of data someone else owns which may be modified.
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public partial struct SliceMutu8
+    {
+        ///Pointer to start of mutable data.
+        IntPtr data;
+        ///Number of elements.
+        ulong len;
+    }
+
+    public partial struct SliceMutu8 : IEnumerable<byte>
+    {
+        public SliceMutu8(GCHandle handle, ulong count)
+        {
+            this.data = handle.AddrOfPinnedObject();
+            this.len = count;
+        }
+        public SliceMutu8(IntPtr handle, ulong count)
+        {
+            this.data = handle;
+            this.len = count;
+        }
+        public byte this[int i]
+        {
+            get
+            {
+                if (i >= Count) throw new IndexOutOfRangeException();
+                var size = Marshal.SizeOf(typeof(byte));
+                var ptr = new IntPtr(data.ToInt64() + i * size);
+                return Marshal.PtrToStructure<byte>(ptr);
+            }
+            set
+            {
+                if (i >= Count) throw new IndexOutOfRangeException();
+                var size = Marshal.SizeOf(typeof(byte));
+                var ptr = new IntPtr(data.ToInt64() + i * size);
+                Marshal.StructureToPtr<byte>(value, ptr, false);
+            }
+        }
+        public byte[] Copied
+        {
+            get
+            {
+                var rval = new byte[len];
+                for (var i = 0; i < (int) len; i++) {
+                    rval[i] = this[i];
+                }
+                return rval;
+            }
+        }
+        public int Count => (int) len;
+        public IEnumerator<byte> GetEnumerator()
+        {
+            for (var i = 0; i < (int)len; ++i)
+            {
+                yield return this[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
 
 
 
