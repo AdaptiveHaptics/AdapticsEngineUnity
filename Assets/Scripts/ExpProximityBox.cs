ec5b5ca50de7e735fcb0731ba4ced1f0be22e8ce
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2023-07-21 12:23:53 -0700

    call OnExitProximity if collider becomes `!activeInHierarchy`

--- Assets/Scripts/ExpProximityBox.cs
+++ Assets/Scripts/ExpProximityBox.cs
@@ -6,19 +6,37 @@ public class ExpProximityBox : MonoBehaviour
 {
 
     BaseExpWithProximity exp;
+    /// OnTriggerExit doesnt get called if the hand tracking collider gets destroyed/disabled/active =false, etc. We just need to handle whatever the leap lib does when hand tracking gets lost
+    HashSet<Collider> collidersInTrigger = new();
 
     private void Start()
     {
         exp = GetComponentInParent<BaseExpWithProximity>();
     }
 
+    private void Update()
+    {
+        collidersInTrigger.RemoveWhere(c => {
+            if (!c.gameObject.activeInHierarchy)
+            {
+                exp.OnExitProximity(c);
+                return true;
+
+            } else
+            {
+                return false;
+            }
+        });
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        collidersInTrigger.Add(other);
         exp.OnEnterProximity(other);
     }
     private void OnTriggerExit(Collider other)
     {
-         /// todo: OnTriggerExit doesnt get called if the hand tracking collider gets destroyed/disabled/active =false, etc. We just need to handle whatever the leap lib does when hand tracking gets lost
+        collidersInTrigger.Remove(other);
         exp.OnExitProximity(other);
     }
     private void OnTriggerStay(Collider other)
